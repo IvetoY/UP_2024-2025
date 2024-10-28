45c4c8d2178c1bb91587501119398757964eb8ed
IvetoY <ivelinabeti@gmail.com>
2024-10-28 21:10:05 +0200

    zad4 pract4

--- pravt_4/zad4.cpp
+++ pravt_4/zad4.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 
 int main (){
-    int x,y,a,b,c;
-    std::cout<<"enter a and b and c"<<std::endl;
-    int n;
+    double x,y,a,b,c;
+    std::cout<<"enter a, b and c"<<std::endl;
+    double n;
+    std::cin>>a>>b>>c;
+    std::cout<<"you enter this "<<a<<"x + "<<b<<"y + "<<c;
     std::cout<<"enter n:"<<std::endl;
     std::cin>>n;
-    for(int )
-    std::cin>>a>>b>>c;
-    for
+    while(n!=0){
+    std::cout<<"enter x:"<<std::endl;
+    std::cin>>x;
+    std::cout<<"enter y:"<<std::endl;
+    std::cin>>y;
+    if((a*x+b*y+c)==0){
+        std::cout<<"true"<<std::endl;
+    }
+    else {
+        std::cout<<"false"<<std::endl;
+    }
+    n--;
+    }
 
     return 0;
 }
